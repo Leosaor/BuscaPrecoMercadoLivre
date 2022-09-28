6da3359d25822206886743558ddd510f0d3fe09e
Leonardo Ramos <leosaor@outlook.com>
2022-09-28 03:07:32 -0300

    Adiciona opção de buscar nome e quantidade de produtos (CLI) e salva em json ordenado por preço

--- index.js
+++ index.js
@@ -1,38 +1,57 @@
 import puppeteer from "puppeteer";
+import fs from "fs";
 
 const url = "https://www.mercadolivre.com.br/"
-const search = "monitor ips"
-let amountSearches = 0;
+let search = process.argv[2] || null; 
+let amountSearches = process.argv[3] || 5;
 
 async function getPrice() {
-    const browser = await puppeteer.launch();	
+    amountSearches = Number(amountSearches);
+    if(search === null) {
+        console.log("Por favor, informe o produto que deseja pesquisar. Use + para separar as palavras.");
+        console.log("Exemplo: node index.js (buscar+produto) (quantidade)");
+        return;
+    }
+    if(isNaN(amountSearches) || amountSearches > 48 || amountSearches < 1) {
+        console.log("Por favor, informe a quantidade de produtos que deseja pesquisar, entre 1 e 48.");
+        console.log("Exemplo: node index.js (buscar+produto) (quantidade)");
+        return;
+    }
+    search = search.replace(/\+/g, " ");
+    const browser = await puppeteer.launch({headless: false});	
     const page = await browser.newPage();
     await page.goto(url);
     await page.waitForSelector('.nav-search-btn');
     await page.type('.nav-search-input', search);
-
     await Promise.all([
         page.waitForNavigation(),
         page.click('.nav-search-btn')
     ]);
-    
-    const products = await page.$$eval('.ui-search-result__image > a', el => el.map(item => item.href));
 
-    let pagina = 0;
+    const products = await page.$$eval('.ui-search-result__image > a', el => el.map(item => item.href));
+    
+    let arrayProducts = [];
+    let link = 0;
+
     for(const product of products) {
-        if(pagina === amountSearches) continue;
+        if(link === amountSearches) continue;
         await page.goto(product);
         await page.waitForSelector('.ui-pdp-title');
-
+        
         const title = await page.$eval('.ui-pdp-title', el => el.innerText);
         const price = await page.$eval('.andes-money-amount__fraction', el => el.innerText);
-        const link = page.url();
+        const priceNumber = Number(price.replace(".", ""));
+        const url = page.url();
 
-        console.log({title, price, link});
+        arrayProducts.push({ title, priceNumber, url });
         
-        pagina++
+        link++;
     }
-
+    arrayProducts.sort((a, b) => a.priceNumber - b.priceNumber);
+    fs.writeFile('products.json', JSON.stringify(arrayProducts), function (err) { 
+        if (err) throw err;
+        console.log('Produto adicionado!');
+    });
     await browser.close();
 }
 
